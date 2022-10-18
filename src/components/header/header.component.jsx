46b7d69b7e88e0b4b04ec78fb1f16beb3e0bbabb
ggez05 <garvit510bhatia@gmail.com>
2022-10-18 22:40:00 +0530

    first commit

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,12 @@
+import './header.styles.css'
+
+const Header = () => {
+    return(
+        <div className='header-wrapper'>
+            <div>
+                <a className='text' href="/">Country List</a>
+            </div>
+        </div>
+    )
+}
+export default Header;
