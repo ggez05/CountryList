d872e929a2df9cec30b24bb0d0f2a1a93e794e94
Garvit <garvit510bhatia@gmail.com>
2022-10-19 08:41:02 +0000

    Update 3 files changed styling

--- src/components/filterbar/filterbar.component.jsx
+++ src/components/filterbar/filterbar.component.jsx
@@ -1,77 +1,72 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import './filterbar.css'
-
-
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./filterbar.css";
 
 const FilterBar = () => {
+  const COUNTRY_URL = "https://restcountries.com/v3.1/all";
 
-    const COUNTRY_URL = 'https://restcountries.com/v3.1/all';
+  const [countryList, setCountry] = useState([]);
+  const [searchString, setSearchString] = useState("");
 
-    const [countryList, setCountry] = useState([]);
-    const [searchString, setSearchString] = useState("");
+  const filtered = countryList.filter((country) => {
+    let name = country.name.common;
+    return name.toLocaleLowerCase().includes(searchString);
+  });
 
-    const filtered = countryList.filter((country) => {
-        let name = country.name.common;
-        return name.toLocaleLowerCase().includes(searchString);
-    });
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await axios.get(`${COUNTRY_URL}`);
+        const Alldata = await response.data;
+        setCountry(Alldata);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchCountries();
+  }, []);
 
-    useEffect(() => {
-        const fetchCountries = async () => {
-            try {
-                const response = await axios.get(`${COUNTRY_URL}`)
-                const Alldata = await response.data
-                setCountry(Alldata)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        fetchCountries()
-    }, [])    
-
-    return (
-        <div className='overall'>
-            <div className='searchbox-wrapperss'>
-            <div className="alert alert-secondary" role="alert">
-            <label htmlFor="search-input" className='label-searchbox'>Search Countries</label>
-            <input type="search" placeholder='Search Country'
-            onChange={(event) => {
-                const searchField = event.target.value.toLocaleLowerCase();
-                setSearchString(searchField);
-    
-            }} 
-            className='region-search' 
-            name='search-input' /> 
+  return (
+    <div className="overall">
+      <div className="searchbox-wrapperss">
+        <label htmlFor="search-input" className="label-searchbox">
+          Search Countries
+        </label>
+        <input
+          type="search"
+          placeholder="Search Country"
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+            setSearchString(searchField);
+          }}
+          className="region-search"
+          name="search-input"
+        />
+      </div>
+      <div className="country-wrapper">
+        {filtered.map((country, i) => {
+          return (
+            <div className="card" key={i}>
+              <img src={country.flags.png} alt={country.name.common} />
+              <div className="card__content">
+                <h1>{country.name?.common}</h1>
+                <p>
+                  <b>Population: </b> {country.population}
+                </p>
+                <p>
+                  <b>Region: </b>
+                  {country.region}
+                </p>
+                <p>
+                  <b>Capital: </b>
+                  {country.capital}
+                </p>
+              </div>
             </div>
-            </div>
-            <div className='country-wrapper'>
-            {
-                filtered.map((country,i)=> {
-                    return(
-                        <div className='card' key={i}>
-                            <img src={country.flags.png} alt={country.name.common} />
-                            <div className="card__content">
-                                <h1>{country.name?.common}</h1>
-                                <p>
-                                    <b>Population: </b> {country.population}
-                                </p>
-                                <p>
-                                    <b>Region: </b>
-                                    {country.region}
-                                </p>
-                                <p>
-                                    <b>Capital: </b>
-                                    {country.capital}
-                                </p>
-                            </div>
-                        </div>
-                    )
-                })
-            }
-            </div>
-        </div>
-    )
-
-}
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 export default FilterBar;
