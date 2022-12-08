32d61745b5a6ec4bda6039804b6ffbfadfbd0257
ggez05 <garvit510bhatia@gmail.com>
2022-12-09 01:07:33 +0530

    restored

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import Header from "./components/header/header.component";
 import FilterBar from "./components/filterbar/filterbar.component";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
-import LoginWhatsapp from "./components/whatsapplogin/loginw";
-import RedirectWhatsapp from "./components/whatsapplogin/redirectw";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/loginwhatsapp" element={<LoginWhatsapp />}></Route>
-          <Route
-            path="/loginwhatsapp/redirect"
-            element={<RedirectWhatsapp />}
-          ></Route>
-
-          <div>
-            <Header />
-            <div className="App">
-              <FilterBar />
-            </div>
-          </div>
-        </Routes>
-      </Router>
-    </>
+    <div>
+      <Header />
+      <div className="App">
+        <FilterBar />
+      </div>
+    </div>
   );
 }
 
